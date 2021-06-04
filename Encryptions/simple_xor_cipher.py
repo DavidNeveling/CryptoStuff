8021e72cd472b428d81f4dd292798edb23e0cd5a
DavidNeveling <d.a.nev@att.net>
2021-06-03 21:28:10 -0500

    added simple xor cipher

---
+++ Encryptions/simple_xor_cipher.py
@@ -0,0 +1,28 @@
+import bit_ops
+
+def encrypt(message, key):
+    key_index = 0
+    encryption = ''
+    for character in message:
+        encryption += chr(ord(character) ^ ord(key[key_index]))
+        if key_index > len(key):
+            key_index = 0
+    return encryption
+
+def decrypt(encryption, key):
+    return encrypt(encryption, key)
+
+def test():
+    message = 'this is a message to encrypt'
+    key = 'face/off'
+    print('message = ' + message)
+    encryption = encrypt(message, key)
+    print('encryption = ' + encryption)
+    decryption = decrypt(encryption, key)
+    print('decryption = ' + decryption)
+
+def main():
+    test()
+
+if __name__ == '__main__':
+    main()
